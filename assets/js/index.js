f2b5173d2986f0801757aaed137dafdda376afa3
Termins55 <kevgame123@gmail.com>
2025-08-22 17:15:48 +0300

    Refactor user card

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,46 +8,53 @@ const user = {
     "https://i.pinimg.com/736x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg",
 };
 
-// 1 + Створити елемент
-// 3 - Задати значення атребутами
-// 4 - Додати класи
-// 5 - Додати контент
-// 6 - Додати обробник на подію
-// 2 + Вбудувати на потрібне місце
-
 const userCardEl = document.createElement("article");
 userCardEl.classList.add("userCard");
 document.body.append(userCardEl);
 
-const userImg = document.createElement("img");
-userImg.src = user.photoSrc;
-userImg.alt = `${user.firstName} ${user.lastName}`;
-userImg.classList.add("userImg");
-userCardEl.append(userImg);
+const userImg = createImg(user);
+const userInfoEl = createUserInfo(user);
+const trashIcon = createTrashIcon();
 
-const userInfoEl = document.createElement("div");
-userInfoEl.classList.add("userInfo");
-userCardEl.append(userInfoEl);
+userCardEl.append(userImg, userInfoEl, trashIcon);
 
-const userNameEl = document.createElement("p");
-userNameEl.classList.add("userName");
-userNameEl.textContent = `${user.firstName} ${user.lastName}`;
-userInfoEl.append(userNameEl);
+// userCardEl.append(createImg(user), createUserInfo(user), createTrashIcon());
 
-const userAgeEl = document.createElement("span");
-userAgeEl.classList.add("userAge");
-userAgeEl.textContent = user.age;
-userInfoEl.append(userAgeEl);
-
-const trashItem = document.createElement("i");
-trashItem.classList.add("fa-solid");
-trashItem.classList.add("fa-trash");
-trashItem.classList.add("trashItem");
-
-function deleteUser(e) {
-  // userCardEl.remove();
-  // e.target.parentElement.remove();
-  e.target.closest(".userCard").remove();
+function createImg({ photoSrc, firstName, lastName }) {
+  const userImg = document.createElement("img");
+  userImg.src = photoSrc;
+  userImg.alt = `${firstName} ${lastName}`;
+  userImg.classList.add("userImg");
+  return userImg;
 }
-trashItem.onclick = deleteUser;
-userCardEl.append(trashItem);
+
+function createUserInfo({ firstName, lastName, age }) {
+  const userInfoEl = document.createElement("div");
+  userInfoEl.classList.add("userInfo");
+
+  const userNameEl = document.createElement("p");
+  userNameEl.classList.add("userName");
+  userNameEl.textContent = `${firstName} ${lastName}`;
+  userInfoEl.append(userNameEl);
+
+  const userAgeEl = document.createElement("span");
+  userAgeEl.classList.add("userAge");
+  userAgeEl.textContent = age;
+  userInfoEl.append(userAgeEl);
+  return userInfoEl;
+}
+
+function createTrashIcon() {
+  const trashIcon = document.createElement("i");
+  trashIcon.classList.add("fa-solid");
+  trashIcon.classList.add("fa-trash");
+  trashIcon.classList.add("trashItem");
+
+  function deleteUser(e) {
+    // userCardEl.remove();
+    // e.target.parentElement.remove();
+    e.target.closest(".userCard").remove();
+  }
+  trashIcon.onclick = deleteUser;
+  return trashIcon;
+}
