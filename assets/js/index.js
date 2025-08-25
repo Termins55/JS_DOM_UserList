368738bb9eec21990c3c767c492865be7a91a637
Termins55 <kevgame123@gmail.com>
2025-08-25 13:43:34 +0300

    Add users list

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,64 @@
 "use strict";
 
-const user = {
-  firstName: "Test1",
-  lastName: "Testovich1",
-  age: 28,
-  photoSrc:
-    "https://i.pinimg.com/736x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg",
-};
+// const user = {
+//   firstName: "Test1",
+//   lastName: "Testovich1",
+//   age: 28,
+//   photoSrc:
+//     "https://i.pinimg.com/736x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg",
+// };
 
-const userCardEl = document.createElement("article");
-userCardEl.classList.add("userCard");
-document.body.append(userCardEl);
+const users = [
+  {
+    firstName: "Test1",
+    lastName: "Testovich1",
+    age: 28,
+    photoSrc: "https://sigc.edu/sigc/ad-sigc/datas/images/userimg.jpg",
+  },
+  {
+    firstName: "Test2",
+    lastName: "Testovich2",
+    age: 31,
+    photoSrc:
+      "https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    firstName: "Test3",
+    lastName: "Testovich3",
+    age: 70,
+    photoSrc:
+      "https://images.pexels.com/photos/573299/pexels-photo-573299.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    firstName: "Test4",
+    lastName: "Testovich4",
+    age: 20,
+    photoSrc:
+      "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    firstName: "Test5",
+    lastName: "Testovich5",
+    age: 28,
+    photoSrc:
+      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
 
-const userImg = createImg(user);
-const userInfoEl = createUserInfo(user);
-const trashIcon = createTrashIcon();
+const userCards = users.map((u) => createUserCard(u));
+document.body.append(...userCards);
 
-userCardEl.append(userImg, userInfoEl, trashIcon);
+function createUserCard(user) {
+  const userCardEl = document.createElement("article");
+  userCardEl.classList.add("userCard");
+
+  const userImg = createImg(user);
+  const userInfoEl = createUserInfo(user);
+  const trashIcon = createTrashIcon();
+
+  userCardEl.append(userImg, userInfoEl, trashIcon);
+  return userCardEl;
+}
 
 // userCardEl.append(createImg(user), createUserInfo(user), createTrashIcon());
 
